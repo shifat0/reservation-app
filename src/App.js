1b0df25a8848259242f429c2820f59e0d11abc27
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-09-12 14:08:09 +0600

    header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import HotelList from "./Pages/HotelList/HotelList";
+import Hotel from "./Pages/Hotel/Hotel";
+
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<HotelList />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Header from '../../Components/Header/Header'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar />
+        <Header />   
+    </>
+  )
+}
+
+export default Home
