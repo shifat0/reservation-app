a4aaedd0ef1136deda310fa0abb71c0941561e97
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-09-12 23:32:58 +0600

    Featured Component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import './Navbar.css'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="navContainer">
-            <span className="logo">Booking</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/">
+          <span className="logo">Booking.com</span>
+        </Link>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import Header from '../../Components/Header/Header'
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
+import Featured from "../../Components/Featured/Featured";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-        <Navbar />
-        <Header />   
+      <Navbar />
+      <Header />
+      <div className="homeContainer"></div>
+      <Featured />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
