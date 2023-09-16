fa267327a83fbd8d92daae843428d79f4daeef49
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-09-16 15:36:36 +0600

    routes updated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import HotelList from "./Pages/HotelList/HotelList";
 import Hotel from "./Pages/Hotel/Hotel";
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/hotels",
+      element: <HotelList />,
+    },
+    {
+      path: "/hotels/:id",
+      element: <Hotel />,
+    },
+  ];
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+          {/* <Route path="/" element={<Home />} />
           <Route path="/hotels" element={<HotelList />} />
-          <Route path="/hotels/:id" element={<Hotel />} />
+          <Route path="/hotels/:id" element={<Hotel />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
